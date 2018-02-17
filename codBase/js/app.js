debcba64e77508858e745df3c8efa52f69be8506
joseacordobac <34005362+joseacordobac@users.noreply.github.com>
2018-02-17 10:35:24 -0500

    Ejecuta operaciones faltan restricciones
    La calculadora esta haciendo las operaciones basicas, dificultades con las restrucciones

--- codBase/js/app.js
+++ codBase/js/app.js
@@ -1,64 +1,141 @@
-//Efecto en los botones.
 
 
-
-//Asignación de valores
-var a;
-var b;
+//Declaramos variables
+var operandoa;
+var operandob;
 var operacion;
 
-//Varibles matemeticas
+    var resultado = document.getElementById('display');
+    var reset = document.getElementById('on');
+    var suma = document.getElementById('mas');
+    var resta = document.getElementById('menos');
+    var multiplicacion = document.getElementById('por');
+    var division = document.getElementById('dividido');
+    var igual = document.getElementById('igual');
+    var uno = document.getElementById('1');
+    var dos = document.getElementById('2');
+    var tres = document.getElementById('3');
+    var cuatro = document.getElementById('4');
+    var cinco = document.getElementById('5');
+    var seis = document.getElementById('6');
+    var siete = document.getElementById('7');
+    var ocho = document.getElementById('8');
+    var nueve = document.getElementById('9');
+    var cero = document.getElementById('0');
+    var signo = document.getElementById('sign')
+    var punto = document.getElementById("punto")
 
-a = 2;
-b = 3;
+    //eventos de click
+    if (resultado.textContent.length < 8) {
+        uno.onclick = function (e) {
+            resultado.textContent = resultado.textContent + "1";
+        }
+    }   
 
-var suma = a+b;
-var resta = a-b;
-var multiplicacion = a*b;
-var division = a/b;
+
+    uno.onclick = function (e) {
+        resultado.textContent = resultado.textContent + "1";
+    }
+
+    dos.onclick = function (e) {
+        resultado.textContent = resultado.textContent + "2";
+
+    }
+    tres.onclick = function (e) {
+        resultado.textContent = resultado.textContent + "3";
+    }
+    cuatro.onclick = function (e) {
+        resultado.textContent = resultado.textContent + "4";
+    }
+    cinco.onclick = function (e) {
+        resultado.textContent = resultado.textContent + "5";
+    }
+    seis.onclick = function (e) {
+        resultado.textContent = resultado.textContent + "6";
+    }
+    siete.onclick = function (e) {
+        resultado.textContent = resultado.textContent + "7";
+    }
+    ocho.onclick = function (e) {
+        resultado.textContent = resultado.textContent + "8";
+    }
+    nueve.onclick = function (e) {
+        resultado.textContent = resultado.textContent + "9";
+    }
+    cero.onclick = function (e) {
+        resultado.textContent = resultado.textContent + "0";
+    }
+
+    punto.onclick = function (e) {
+        resultado.textContent = resultado.textContent + ".";
+    }
+
+    signo.onclick = function (e) {
+        resultado.textContent = resultado.textContent * "-1"
+    }     
+        
+    //Operaciones
+    reset.onclick = function (e) {
+        resetear();
+    }
+    suma.onclick = function (e) {
+        operandoa = resultado.textContent;
+        operacion = "+";
+        limpiar();
+    }
+    resta.onclick = function (e) {
+        operandoa = resultado.textContent;
+        operacion = "-";
+        limpiar();
+    }
+    multiplicacion.onclick = function (e) {
+        operandoa = resultado.textContent;
+        operacion = "*";
+        limpiar();
+    }
+    division.onclick = function (e) {
+        operandoa = resultado.textContent;
+        operacion = "/";
+        limpiar();
+    }
+    igual.onclick = function (e) {
+        operandob = resultado.textContent;
+        resolver();
+    }
 
 
-//Funcion para operacion.
-function inicio() {
-    // Variables par apantalla de resultados
-     var resultado = document.getElementById("display");
-     var on = document.getElementById("on");
-     var signo = document.getElementById ("sign");
-     var dividir = document.getElementById("dividido");
-     var multiplicar = document.getElementById ("por");
-     var restar = document.getElementById("menos");
-     var sumar = document.getElementById("mas");
-     var punto = document.getElementById("punto");
-     var igual = document.getElementById ("igual");
-     var uno = document.getElementById ("1");
-     var dos = document.getElementById ("2");
-     var tres = document.getElementById ("3");
-     var cuatro = document.getElementById("4");
-     var cinco = document.getElementById("5");
-     var seis = document.getElementById("6");
-     var siete = document.getElementById("7");
-     var ocho = document.getElementById("8");
-     var nueve = document.getElementById("9");
-     var cero = document.getElementById("0 ");   
+function limpiar() {
+    resultado.textContent = "";
 }
 
-cero = 0;
-uno = 1;
-dos = 2;
-tres = 3
-cuatro = 4;
-cinco = 5;
-seis =  6;
-siete = 7;
-ocho = 8;
-nueve = 9;
-
-function nuno(params) {
-    document.getElementById("display").innerHTML += uno
+function resetear() {
+    resultado.textContent = "0";
+    operandoa = 0;
+    operandob = 0;
+    operacion = "";
 }
-document.getElementById("1").onclick = function () {nuno()};
 
-function ndos(params) {
-    document.getElementById("display").innerHTML += dos
+function resolver() {
+    var res = 0;
+    switch (operacion) {
+        case "+":
+            res = parseFloat(operandoa) + parseFloat(operandob);
+            break;
+
+        case "-":
+            res = parseFloat(operandoa) - parseFloat(operandob);
+            break;
+
+        case "*":
+            res = parseFloat(operandoa) * parseFloat(operandob);
+            break;
+
+        case "/":
+            res = parseFloat(operandoa) / parseFloat(operandob);
+            break;
+    }
+    resetear();
+    resultado.textContent = res;
 }
-document.getElementById("2").onclick = function () { ndos() };
+
+
